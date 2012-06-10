d7c413ff4bac98e5e52f98f6a95334ecaa1ec63d
Marco Rogers <marco.rogers@gmail.com>
2012-06-10 15:15:35 -0700

    custom logging stream

---
+++ logstream.js
@@ -0,0 +1,19 @@
+var Stream = require('stream'),
+  util = require('util');
+
+var LogStream = function() {
+  this.readable = true;
+  this.writable = true;
+};
+util.inherits(LogStream, Stream);
+LogStream.prototype.write = function(data) {
+  console.log('written: ' + data.toString());
+  this.emit('data', data);
+};
+LogStream.prototype.end = function(data) {
+  if(data) { this.emit('data', data); }
+
+  this.emit('end');
+};
+
+module.exports = LogStream;
--- streams.js
+++ streams.js
@@ -1,7 +1,11 @@
 var Stream = require('stream'),
   fs = require('fs'),
-  http = require('http');
+  http = require('http'),
+  LogStream = require('./logstream');
 
 http.createServer(function(request, response) {
-  fs.createReadStream('./large-file.txt').pipe(response);
+  var logstream = new LogStream();
+  fs.createReadStream('./large-file.txt').pipe(logstream).pipe(response);
 }).listen(8080);
+
+console.log('Server listening on port 8080\n');
