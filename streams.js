88b19fceccd063cb2231ab1be4240a47030dfd10
Marco Rogers <marco.rogers@gmail.com>
2012-06-17 13:45:13 -0700

    custom gzip stream with collector

--- streams.js
+++ streams.js
@@ -1,17 +1,25 @@
 var Stream = require('stream'),
   fs = require('fs'),
   http = require('http'),
-  GZipStream = require('./gzipstream');
+  GZipStream = require('./gzipstream'),
+  Collector = require('./collector');
 
 http.createServer(function(request, response) {
-  var gzipstream = new GZipStream();
-
-  response.setHeader('Content-Type', 'text/plain');
-  response.setHeader('Content-Encoding', 'gzip');
+  var gzipper = new GZipStream(),
+      collector = new Collector();
 
   fs.createReadStream('./large-file.txt')
-    .pipe(gzipstream)
-    .pipe(response);
+    .pipe(gzipper)
+    .pipe(collector);
+
+  collector.on('end', function() {
+    response.writeHead(200, {
+      'Content-Encoding': 'gzip',
+      'Content-Type': 'text/plain',
+      'Content-Length': collector.getBytesWritten()
+    });
+    response.end(collector.getData());
+  });
 }).listen(8080);
 
 console.log('Server listening on port 8080\n');
