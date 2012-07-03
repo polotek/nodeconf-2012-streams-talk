b8965cd1524a6744f4c1366669cecaedbb414cd7
Marco Rogers <marco.rogers@gmail.com>
2012-07-02 19:41:11 -0700

    add upload example

---
+++ upload-example2.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+  , http = require('http')
+  , formidable = require('formidable')
+  , forHumans = require('./for-humans.js');
+
+http.createServer(function(request, response) {
+  if (request.url == '/upload' && request.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+    form.onPart = function(part) {
+      part.pipe(fs.createWriteStream('./files/test.json'));
+    }
+    form.on('end', function() {
+      console.log(forHumans(process.memoryUsage()));
+      response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      response.end('We got the junk you uploaded. Thanks.');
+    });
+    return form.parse(request);
+  }
+
+  response.setHeader('Content-Type', 'text/html; charset=utf-8');
+  fs.createReadStream('./upload-example-form.html').pipe(response);
+}).listen(8080);
+
+console.log('Server ' + process.pid + ' listening on port 8080\n');
