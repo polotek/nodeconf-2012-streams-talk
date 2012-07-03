2f8c29196f38c6f0e656973826fb8d1beb42d651
Marco Rogers <marco.rogers@gmail.com>
2012-07-02 20:35:45 -0700

    bring back simpler collector stream

--- streams/collector2.js
+++ streams/collector2.js
@@ -3,19 +3,22 @@ var Stream = require('stream').Stream
 
 var Collector = function() {
   this._bytesWritten = 0;
-  this._data = '';
+  this._data = null;
   this.readable = true;
   this.writable = true;
 };
 util.inherits(Collector, Stream);
 Collector.prototype.write = function(data) {
   if(typeof data == 'string') {
+    if(!this._data) { this._data = ''; }
+    this._data += data.toString();
     this._bytesWritten += Buffer.byteLength(data);
   } else {
+    // Buffer
+    if(!this._data) { this._data = []; }
+    this._data.push(data);
     this._bytesWritten += data.length;
   }
-
-  this._data += data.toString();
 
   this.emit('data', data);
 };
@@ -24,9 +27,19 @@ Collector.prototype.end = function(data) {
 
   this.readable = false;
   this.writable = false;
+
+  this.getData();
   this.emit('end');
 };
 Collector.prototype.getData = function() {
+  if(this.writable) {
+    throw new Error('Cannot get data before the end of the stream');
+  }
+
+  if(Array.isArray(this._data)) {
+    this._data = Buffer.concat(this._data, this.getBytesWritten());
+  }
+
   return this._data;
 };
 Collector.prototype.getBytesWritten = function() {
