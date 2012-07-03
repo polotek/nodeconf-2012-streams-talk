7070af686eafe98a9818e05fca578a84c8fd4444
Marco Rogers <marco.rogers@gmail.com>
2012-07-02 20:49:06 -0700

    comment gzipper

--- streams/gzipstream.js
+++ streams/gzipstream.js
@@ -3,23 +3,35 @@ var Stream = require('stream').Stream,
   spawn = require('child_process').spawn;
 
 var GZipStream = function() {
+  // readable stream
   this.readable = true;
+  // writable stream
   this.writable = true;
 
   this._gzipper = spawn('gzip');
+  // emit data at some point
   this._gzipper.stdout.on('data', this.emit.bind(this, 'data'));
+  // emit end at some point
   this._gzipper.on('exit', this.emit.bind(this, 'end'));
 };
+
+// this is a custom stream object
 util.inherits(GZipStream, Stream);
+
+// implement write, process some data
 GZipStream.prototype.write = function(data) {
   if(this._gzipper.stdin.writable) {
     this._gzipper.stdin.write(data);
   }
 };
+
+// implement end
 GZipStream.prototype.end = function(data) {
+  // shut down processing
   this.readable = false;
   this.writable = false;
 
+  // clean up
   this._gzipper.stdin.end();
   this._gzipper = null;
 };
